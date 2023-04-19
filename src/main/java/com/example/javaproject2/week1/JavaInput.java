a0b2c494d3c59000c196f08a916d5c0d238538e6
kimmingyun <mingyun8646@nate.com>
2023-04-19 14:22:48 +0900

    feat: 입력 출력 - JavaInputTest

---
+++ src/main/java/com/example/javaproject2/week1/JavaInput.java
@@ -0,0 +1,22 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class JavaInput {
+    public void readAChar() throws IOException {
+        // InputStreamReader라는 클래스를 is라는 이름으로 선언
+        InputStreamReader is = new InputStreamReader(System.in);
+        System.out.println(is.read());
+
+    }
+    public void readTwoChars() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        int asciiCode1 = is.read();
+        int asciiCode2 = is.read();
+
+        System.out.println(asciiCode1);
+        System.out.println(asciiCode2);
+    }
+}
+
+
---
+++ src/main/java/com/example/javaproject2/week1/JavaInputTest.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    /*
+    param1 : ~~
+    param2 : ~~~
+     */
+    public static void main(String[] args) throws IOException, IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars();
+    }
+}
+

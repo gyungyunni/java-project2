9468c64b9dce8eaca396ef32722defc2ddc2db91
kimmingyun <mingyun8646@nate.com>
2023-04-20 13:39:34 +0900

    feat: 세 개의 숫자 평균 구하기까지

---
+++ src/main/java/com/example/javaproject2/week1/day4/FloatFormatEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 3.389);
+        System.out.printf("%.1f\n", 3.389); //첫째자리까지 반올림
+        System.out.printf("%.2f\n", 3.389); //둘째짜리까지 반올림
+
+    }
+}

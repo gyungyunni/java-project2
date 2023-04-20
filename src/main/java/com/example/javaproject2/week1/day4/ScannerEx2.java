c8c57f4677c3d41076e34556b33a696b94b0a10c
kimmingyun <mingyun8646@nate.com>
2023-04-20 09:32:23 +0900

    feat: Scanner로 2개 입력받기

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerEx2.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+public class ScannerEx2 {
+    public void inputTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}

f85e94acdc754a98eda8f5b757356eb4d0c3451a
kimmingyun <mingyun8646@nate.com>
2023-04-20 11:28:27 +0900

    feat: 구구단 출력까지 - printf

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintWhy.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintWhy {
+    void print(){
+        System.out.println("why");
+    }
+    public static void main(String[] args) {
+
+    }
+}

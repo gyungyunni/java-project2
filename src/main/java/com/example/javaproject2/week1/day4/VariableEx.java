a0e1c0a643c4cd7e3ff985e092062319330f55fd
kimmingyun <mingyun8646@nate.com>
2023-04-20 15:15:13 +0900

    feat: 변수 선언과 초기화

---
+++ src/main/java/com/example/javaproject2/week1/day4/VariableEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0; //초기화
+        PrintHello printHello;
+        printHello = new PrintHello(); // 초기화
+        System.out.println(iVal);
+        printHello.print();
+    }
+}

6f2895ebdb82a063b617f0f30700cee7b751e42b
kimmingyun <mingyun8646@nate.com>
2023-04-25 00:27:56 +0900

    feat: Codeup1031-1037

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1034.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.valueOf(sc.next(), 8);
+        System.out.print(num);
+    }
+}

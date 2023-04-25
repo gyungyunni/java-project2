acd62a9839276e912e59694ee2db0fe062c2888d
kimmingyun <mingyun8646@nate.com>
2023-04-25 15:44:19 +0900

    feat: week2-day2

---
+++ src/main/java/com/example/javaproject2/week2/day2/StringTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2.day2;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("1" + 1);
+        System.out.println(3+5+"7"+6);
+        System.out.println("3"+3+5+6);
+    }
+}

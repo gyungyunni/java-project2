40488399d5893c3ed2a240a903cfe12182a4a35c
kimmingyun <mingyun8646@nate.com>
2023-04-26 10:07:29 +0900

    feat: week2-day3

---
+++ src/main/java/com/example/javaproject2/week2/day2/test.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day2;
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                double num = sc.nextDouble();
+
+                System.out.println(num);
+            }
+
+        }
+
---
+++ src/main/java/com/example/javaproject2/week2/day3/ReferenceTypeVariable.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2.day3;
+
+public class ReferenceTypeVariable {
+    public static void main(String[] args) {
+        Student student = new Student(); //student 타입, new를 써서 Student 객체를 인스턴스로
+        Student[] students = new Student[30]; // Student타입의 Student[] 배열
+
+        //primitive 타입은 클래스가 아님
+    }
+}

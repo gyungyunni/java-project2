c3f732ba6a8de4a360babb240b43e4c66797a967
kimmingyun <mingyun8646@nate.com>
2023-04-27 13:04:29 +0900

    feat: Codeup 1047-1060

--- src/main/java/com/example/javaproject2/codeup/codeup1000/Codeup1057.java
+++ src/main/java/com/example/javaproject2/codeup/codeup1000/Codeup1057.java
@@ -7,7 +7,7 @@ public class Codeup1057 {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        boolean result = num1 ==1 & num2==1 || num1 ==0 & num2==0;
+        boolean result = num1 == 1 & num2 == 1 || num1 == 0 & num2 == 0;
         if(result)
             System.out.println("1");
         else

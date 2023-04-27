3b8d42de6e04f1b8c61986ddddc06a1da0ac6f9d
kimmingyun <mingyun8646@nate.com>
2023-04-27 10:16:29 +0900

    feat: Week2-day4

---
+++ src/main/java/com/example/javaproject2/week2/day4/ArraySum.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {7, 5, 8, 9};
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+        System.out.printf("answer: %d", answer);
+    }
+}

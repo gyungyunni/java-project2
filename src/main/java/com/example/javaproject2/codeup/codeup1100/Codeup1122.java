f884887b11f92e321536093034cdd2f14849048e
kimmingyun <mingyun8646@nate.com>
2023-04-28 15:22:23 +0900

    feat: Codeup 1122-1124

---
+++ src/main/java/com/example/javaproject2/codeup/codeup1100/Codeup1122.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup.codeup1100;
+
+import java.util.Scanner;
+
+public class Codeup1122 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+
+        System.out.printf("%d %d\n",num1 / 60, num1 % 60);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/codeup1100/Codeup1123.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup.codeup1100;
+
+import java.util.Scanner;
+
+public class Codeup1123 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        float num2 = (float) 9 / 5 * (float)num1 +32;
+        System.out.printf("%.3f\n",num2);;
+    }
+}

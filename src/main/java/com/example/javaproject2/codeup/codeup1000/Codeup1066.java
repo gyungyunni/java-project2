44cdd1c0f818793c8a4ec9dc20725bf292fe0939
kimmingyun <mingyun8646@nate.com>
2023-04-28 09:40:02 +0900

    feat: Codeup 1065-1067

---
+++ src/main/java/com/example/javaproject2/codeup/codeup1000/Codeup1066.java
@@ -0,0 +1,20 @@
+package com.example.javaproject2.codeup.codeup1000;
+
+import java.util.Scanner;
+
+public class Codeup1066 {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 3; i++){
+            array[i] = sc.nextInt();
+        }
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % 2 == 0)
+                System.out.println("even");
+            else
+                System.out.println("odd");
+        }
+    }
+}
+

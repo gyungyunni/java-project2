dc021de5cb5d2bde585a58e0310bfe9f2050d4be
kimmingyun <mingyun8646@nate.com>
2023-04-28 14:27:13 +0900

    feat: Codeup 1068-1070, 1116-1121

---
+++ src/main/java/com/example/javaproject2/codeup/codeup1100/Codeup1118.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup.codeup1100;
+
+import java.util.Scanner;
+
+public class Codeup1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+
+        System.out.printf("%.1f\n",(num1*num2)*0.5);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/codeup1100/Codeup1120.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup.codeup1100;
+
+import java.util.Scanner;
+
+public class Codeup1120 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+        float num3 = sc.nextFloat();
+
+        System.out.printf("%.2f\n",(num1+num2+num3)/3);
+    }
+}

e91a45386fa5cd2dab5bdcb340759c15a592750d
kimmingyun <mingyun8646@nate.com>
2023-05-02 16:13:58 +0900

    feat: Week3-day2

---
+++ src/main/java/com/example/javaproject2/week3/day2/ForLoopUntil.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week3.day2;
+
+public class ForLoopUntil {
+    public static void main(String[] args) {
+        // (int i = 2; 2 * i <= 18; i++)
+        // (int i = 2; 1 == 1; i++)
+        for (int i = 2; i * i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
